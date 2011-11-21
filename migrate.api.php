78eec1b7a46afff99ab9af56976825694ac2afb4
Mike Ryan <mike.ryan@acquia.com>
2011-11-21 13:40:59 -0500

    Issue #1337810 by joachim,mikeryan: Documented hooks in migrate.api.php

--- migrate.api.php
+++ migrate.api.php
@@ -2,7 +2,26 @@
 
 /**
  * @file
- * Documentation for Migrate module's API.
+ * Documentation for hooks defined by Migrate.
  */
 
-/* TBD for Migrate 2.0 */
+/**
+ * Registers your module as an implementor of Migrate-based classes.
+ */
+function hook_migrate_api() {
+  $api = array(
+    'api' => 2,
+  );
+  return $api;
+}
+
+/**
+ * Provides text to be displayed at the top of the dashboard page (migrate_ui).
+ */
+function hook_migrate_overview() {
+  return t('<p>Listed below are all the migration processes defined for migration
+    of our old site to Drupal. Open issues applying to specific migrations
+    can be viewed by clicking the migration name. Also, details on how each
+    migration will behave when incrementally migrated are provided.</p>
+    <p><a href="http://issuetracker.example.com/?project=migration&status=open">Open migration tickets</a></p>');
+}
